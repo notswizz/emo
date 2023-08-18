71b22d0f0f17413307713675c7406b780c2ab04b
Your Name <you@example.com>
2023-08-18 02:38:17 -0400

    firebase connection

--- script.js
+++ script.js
@@ -1,3 +1,22 @@
+// Import the functions you need from the Firebase SDK
+import { initializeApp } from "firebase/app";
+import { getFirestore, doc, updateDoc, getDoc } from "firebase/firestore";
+
+const firebaseConfig = {
+    apiKey: "AIzaSyC8U0HVMHjLxYe4rvJA3o8o7oxKL8PL4z8",
+    authDomain: "emoji-e7746.firebaseapp.com",
+    projectId: "emoji-e7746",
+    storageBucket: "emoji-e7746.appspot.com",
+    messagingSenderId: "209213502061",
+    appId: "1:209213502061:web:6ad8eab6216dd70ccf4564"
+  };
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
+
+
+
 const emojis = ['💧'];
 const gridSize = 10;
 let selectedEmoji = null;
@@ -105,6 +124,47 @@ function isEmojiBought(emojiId) {
     return boughtEmojis.includes(emojiId);
 }
 
+// Function to update emoji's bought status in Firestore
+async function markEmojiAsBought(emojiId) {
+    const emojiRef = doc(db, "emojis", emojiId);
+    await updateDoc(emojiRef, { bought: true });
+}
+
+// Function to update emoji's inspected status in Firestore
+async function markEmojiAsInspected(emojiId) {
+    const emojiRef = doc(db, "emojis", emojiId);
+    await updateDoc(emojiRef, { inspected: true });
+}
+
+// Create grid
+for (let i = 0; i < gridSize; i++) {
+    for (let j = 0; j < gridSize; j++) {
+        const gridItem = document.createElement('div');
+        gridItem.classList.add('grid-item');
+        gridItem.innerText = emojis[Math.floor(Math.random() * emojis.length)];
+        gridItem.dataset.row = i;
+        gridItem.dataset.col = j;
+        const emojiId = `${i}-${j}`;
+        gridItem.dataset.id = emojiId;
+
+        // Check if the emoji was inspected or bought and set the background color accordingly
+        const emojiRef = doc(db, "emojis", emojiId);
+        const emojiSnapshot = await getDoc(emojiRef);
+
+        if (emojiSnapshot.exists()) {
+            const emojiData = emojiSnapshot.data();
+            if (emojiData.bought) {
+                gridItem.style.backgroundColor = 'green';
+            } else if (emojiData.inspected) {
+                gridItem.style.backgroundColor = 'red';
+            }
+        }
+
+        gridItem.addEventListener('click', handleClick);
+        gridContainer.appendChild(gridItem);
+    }
+}
+
 // Create grid
 for (let i = 0; i < gridSize; i++) {
     for (let j = 0; j < gridSize; j++) {
@@ -127,6 +187,24 @@ for (let i = 0; i < gridSize; i++) {
     }
 }
 
+inspectBtn.addEventListener('click', async () => {
+    if (selectedEmoji) {
+        selectedEmoji.style.backgroundColor = 'red';
+        await markEmojiAsInspected(selectedEmoji.dataset.id);
+        location.reload(); // Reload the page
+    }
+});
+
+buyBtn.addEventListener('click', async () => {
+    if (selectedEmoji) {
+        selectedEmoji.style.backgroundColor = 'green';
+        await markEmojiAsBought(selectedEmoji.dataset.id);
+        location.reload(); // Reload the page
+    }
+});
+
+
 inspectBtn.addEventListener('click', handleInspect);
 buyBtn.addEventListener('click', handleBuy);
 window.addEventListener('keydown', handleKeyDown);
+
